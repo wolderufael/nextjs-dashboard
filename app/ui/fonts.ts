73cc3894452c01dd6835166dbc297fd23242cb5d
wolderufael <wolderuphaelkassahun@gmail.com>
2025-06-10 08:50:01 +0300

    initial commit

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Lusitana } from "next/font/google";
+import { Inter } from "next/font/google";
+
+export const lusitana = Lusitana({
+  weight: ["400", "700"],
+  subsets: ["latin"],
+});
+
+export const inter = Inter({ subsets: ["latin"] });
